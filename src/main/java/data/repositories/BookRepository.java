122f24404aa52969587bae053396ea35bf2a2f08
Sergey Tikhov <tihov21@mail.ru>
2018-12-11 12:25:17 +0300

    Добавил уточнение в sql запросы BookRepository с указанием схемы, в которой находится таблица

--- src/main/java/data/repositories/BookRepository.java
+++ src/main/java/data/repositories/BookRepository.java
@@ -14,24 +14,24 @@ import data.entities.Book;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-	@Query(value = "SELECT * FROM books WHERE (title LIKE :search) OR (author LIKE :search) OR (isbn LIKE :search)", nativeQuery = true )
+	@Query(value = "SELECT * FROM book_store.books WHERE (title LIKE :search) OR (author LIKE :search) OR (isbn LIKE :search)", nativeQuery = true )
 	public List<Book> searchBooks(@Param("search") String search );
 	
 	public Page<Book> findByTitleContainingOrAuthorContainingOrIsbnContaining(String title, String author, String isbn, Pageable pageable);
 		
-	@Query(value = "SELECT DISTINCT books.Genre_One FROM books", nativeQuery = true )
+	@Query(value = "SELECT DISTINCT book_store.books.Genre_One FROM book_store.books", nativeQuery = true )
 	public List<String> getDistinctGenreOne();
 	
-	@Query(value = "SELECT DISTINCT books.Genre_Two FROM books", nativeQuery = true )
+	@Query(value = "SELECT DISTINCT book_store.books.Genre_Two FROM book_store.books", nativeQuery = true )
 	public List<String> getDistinctGenreTwo();
 		
-	@Query(value = "SELECT DISTINCT books.Genre_Two FROM books WHERE books.Genre_One = :genreOne", nativeQuery = true )
+	@Query(value = "SELECT DISTINCT book_store.books.Genre_Two FROM books WHERE book_store.books.Genre_One = :genreOne", nativeQuery = true )
 	public List<String> getInnerGenreForEachOuterGenre(@Param("genreOne") String genreOne);
 	
-	@Query(value = "SELECT DISTINCT books.Genre_One FROM books WHERE books.Genre_Two = :genreTwo", nativeQuery = true )
+	@Query(value = "SELECT DISTINCT book_store.books.Genre_One FROM books WHERE book_store.books.Genre_Two = :genreTwo", nativeQuery = true )
 	public String getOuterGenreForInnerGenre(@Param("genreTwo") String genreTwo);
 	
-	@Query(value = "SELECT * FROM books WHERE (Genre_One LIKE :genre) OR (Genre_Two LIKE :genre) ", nativeQuery = true)
+	@Query(value = "SELECT * FROM book_store.books WHERE (Genre_One LIKE :genre) OR (Genre_Two LIKE :genre) ", nativeQuery = true)
 	public Page<Book> getBooksByGenre(@Param("genre") String genre, Pageable pageable);
 	
 	public Page<Book> findByGenreOneOrGenreTwo(String genreOne, String genreTwo, Pageable pageable);
