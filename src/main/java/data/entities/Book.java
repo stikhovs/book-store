daa7c2751a9b4f8c32afb5039a635575b9ca0a2a
Sergey Tikhov <tihov21@mail.ru>
2018-12-13 13:27:19 +0300

    Добавил возможность добавления новых книг в БД. Возможность редактирования уже существующих книг. Создал новую таблицу со слайдами для главной страницы. Добавил возможность добавления существующих книг в слайдер.

--- src/main/java/data/entities/Book.java
+++ src/main/java/data/entities/Book.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class Book {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.TABLE)
 	@Column(name = "book_id")
 	private long bookId;
 	
